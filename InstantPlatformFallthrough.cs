7e39810a029a8390b6e0c4118100a1412d083287
diniamo <diniamo69@gmail.com>
2023-06-20 19:52:58 +0200

    Initial commit

---
+++ InstantPlatformFallthrough.cs
@@ -0,0 +1,48 @@
+using static Mono.Cecil.Cil.OpCodes;
+using Terraria;
+using MonoMod.Cil;
+using System;
+using Terraria.ModLoader;
+using Mono.Cecil.Cil;
+using System.Text;
+using System.IO;
+
+namespace InstantPlatformFallthrough
+{
+	public class InstantPlatformFallthrough : Mod
+	{
+        public override void Load()
+        {
+            IL.Terraria.Player.Update += il =>
+            {
+                try
+                {
+                    var c = new ILCursor(il);
+                    c.GotoNext(MoveType.After,
+                        i => i.MatchStfld<Player>(nameof(Player.slideDir)),
+                        i => i.Match(Ldc_I4_0),
+                        i => i.Match(Stloc_S),
+                        i => i.Match(Ldarg_0),
+                        i => i.MatchLdfld<Player>(nameof(Player.controlDown)),
+                        i => i.Match(Stloc_S)
+                    );
+
+                    var label = il.DefineLabel();
+
+                    c.Emit(Ldloc_S, (byte)13); // fallThrough
+                    c.Emit(Brfalse_S, label);
+                    c.Emit(Ldc_I4_1);
+                    c.Emit(Stloc_S, (byte)12); // ignorePlats
+
+                    c.MarkLabel(label);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            };
+
+            base.Load();
+        }
+    }
+}
